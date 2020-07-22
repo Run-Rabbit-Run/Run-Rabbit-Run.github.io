dbd6c0204ed61a4d0e84ad4e3971bdf01ae68686
rabbit <barateligeorge@yandex.ru>
2020-07-22 14:51:09 +0300

    .

--- projects/potency/js/script.js
+++ projects/potency/js/script.js
@@ -1,18 +1,39 @@
 'use strict';
 
+// Слайдер
 (function () {
-  $(document).ready(function(){
+  $(document).ready(function () {
     $('.slider__wrapper').slick({
       adaptiveHeight: true,
-      // autoplay: true,
-      // autoplaySpeed: 8000
+      autoplay: true,
+      autoplaySpeed: 7000
     });
   });
 })();
 
+// Поле ввода телефона
 (function () {
   var phoneMask = IMask(
-    document.getElementById('tel'), {
-      mask: '+{7}(000)000-00-00'
+      document.getElementById('tel'), {
+        mask: '+{7}(000)000-00-00'
+      });
+})();
+
+// Таймер
+(function () {
+  $(function(){
+
+    var note = $('#note'),
+      ts = new Date(2020, 0, 1),
+      newTime = true;
+
+    if((new Date()) > ts){
+      ts = (new Date()).getTime() + 1*12*59*60*1000;
+      newTime = false;
+    }
+
+    $('#countdown').countdown({
+      timestamp	: ts
     });
+  });
 })();
